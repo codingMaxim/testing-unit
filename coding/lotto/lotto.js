77081c6ab784b26edc1b12486dce7b4ed45a4ced
codingMaxim <maxim@tjupalow.de>
2022-04-27 11:27:30 +0200

    feat: lotto

--- coding/lotto/lotto.js
+++ coding/lotto/lotto.js
@@ -1,3 +1,12 @@
 function randomLottoTicket() {
-  // Add your code here
+  let arr = [];
+  let min = Math.ceil(1);
+  let max = Math.floor(49);
+  while (arr.length < 6) {
+    let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    if (arr.indexOf(randomNumber) === -1) {
+      arr.push(randomNumber);
+    }
+  }
+  return arr;
 }
